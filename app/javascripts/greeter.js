0dd71fba3d5f40afcce984bb8ebe6c7c6442241a
acoutts <andrewjohncoutts@gmail.com>
2017-07-15 18:13:50 -0400

    Update UI

--- app/javascripts/greeter.js
+++ app/javascripts/greeter.js
@@ -27,12 +27,12 @@ window.App = {
     // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function(err, accs) {
       if (err != null) {
-        alert("There was an error fetching your accounts.");
+        console.log("There was an error fetching your accounts.");
         return;
       }
 
       if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
+        console.log("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
         return;
       }
 
@@ -89,11 +89,13 @@ window.App = {
 window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.log("Sucessfully detected web3 provider.")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+    alert("No web3 detected. Please ensure you're visiting this page from a compatibile web3 browser such as Chrome with the MetaMask extension or from a Mist wallet.");
+    console.warn("No web3 detected. Falling back to http://localhost:8545.");
+    self.setStatus("No web3 detected. Please visit from a web3 enabled browser such as Mist or MetaMask.");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   }
